7fa309087c4897f7b91d311c57657e48c70c55a8
leenawon <nawon5154@gmail.com>
2021-12-21 18:43:02 +0900

    firebase에서 사용자의 상태에 대한 변화를 감지했을 때 state값이 바뀌도록 수정

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,29 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import AppRouter from 'components/Router';
 import { firebaseAuth } from 'myFirebase';
 import 'css/App.css';
 
 function App() {
-  const [firebaseListen, setFirebaseListen] = useState(firebaseAuth.currentUser);
+  // firebase data를 읽어들였는지 여부에 대한 state
+  const [firebaseListen, setFirebaseListen] = useState(false);
+  const [login, setLogin] = useState(false);
+
+  useEffect(() => {
+    // Auth에 대한 변화를 감지함
+    firebaseAuth.onAuthStateChanged((user) => {
+      if(user) {
+        setLogin(true);
+      } else {
+        setLogin(false);
+      }
+      setFirebaseListen(true);
+    });
+  }, []);
+
   return (
     <div className="app">
       <h3 className="title">🌳 Book-ggum-na-mu 🌳</h3>
-      {firebaseListen ? <AppRouter login={firebaseListen}/> : "로그인 정보를 불러오는 중..."}
+      {firebaseListen ? <AppRouter login={login}/> : "로그인 정보를 불러오는 중..."}
     </div>
   );
 }
