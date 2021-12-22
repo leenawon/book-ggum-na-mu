fe52e70e12df7f5e430689ca4e46a10482c03ad1
leenawon <nawon5154@gmail.com>
2021-12-23 01:52:17 +0900

    사용자에 대한 변화 감지 후 사용자 정보를 state에 저장하고 Router에 props로 전달

--- src/components/App.js
+++ src/components/App.js
@@ -6,15 +6,15 @@ import 'css/App.css';
 function App() {
   // firebase data를 읽어들였는지 여부에 대한 state
   const [firebaseListen, setFirebaseListen] = useState(false);
-  const [login, setLogin] = useState(false);
+  const [userObject, setUserObject]= useState(null);
 
   useEffect(() => {
     // Auth에 대한 변화를 감지함
     firebaseAuth.onAuthStateChanged((user) => {
       if(user) {
-        setLogin(true);
+        setUserObject(user);
       } else {
-        setLogin(false);
+        setUserObject(null);
       }
       setFirebaseListen(true);
     });
@@ -23,7 +23,7 @@ function App() {
   return (
     <div className="app">
       <h3 className="title">🌳 Book-ggum-na-mu 🌳</h3>
-      {firebaseListen ? <AppRouter login={login}/> : "로그인 정보를 불러오는 중..."}
+      {firebaseListen ? <AppRouter login={Boolean(userObject)} userObject={userObject}/> : "로그인 정보를 불러오는 중..."}
     </div>
   );
 }
