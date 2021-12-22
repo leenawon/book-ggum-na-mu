b915c71c19656f05f1cad9fd5f41ff8ad119dfde
leenawon <nawon5154@gmail.com>
2021-12-23 01:49:35 +0900

    Home page에 ContentForm과 함께 database 내용 불러와서 보여주도록 작성

---
+++ src/pages/Home.js
@@ -0,0 +1,35 @@
+import { fireDatabase } from 'myFirebase';
+import React, { useEffect, useState } from 'react';
+import { collection, query, orderBy, onSnapshot } from 'firebase/firestore';
+import ContentForm from 'components/ContentForm';
+
+function Home() {
+  const [contents, setContents] = useState([]);
+  
+  // database에 있는 데이터 내림차순 정렬 후 state에 저장
+  useEffect(() => {
+    const documents = query(collection(fireDatabase, "contents"), orderBy("createdAt", "desc"));
+    // onSnapshot은 데이터베이스의 변화를 실시간으로 알려줌
+    onSnapshot(documents, (snapshot) => {
+      const documentArray = snapshot.docs.map((document) => ({
+        id: document.id,
+        ...document.data()
+      }));
+      setContents(documentArray);
+    });
+    return () => setContents([]);
+  }, []);
+
+  return (
+    <div>
+      <ContentForm/>
+      <div>
+        {contents.map((content) => (
+          <h4>{content.text}</h4>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Home;
