53bdaac636af6736ec924c63de4040d6fdbbc02e
leenawon <nawon5154@gmail.com>
2021-12-24 02:04:56 +0900

    글 수정을 위해서 state 및 form 추가하고 수정 기능 제작

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,9 +1,12 @@
 import { fireDatabase } from 'myFirebase';
-import React from 'react';
-import { doc, deleteDoc } from "firebase/firestore";
+import React, { useState } from 'react';
+import { doc, deleteDoc, updateDoc } from "firebase/firestore";
 import style from 'css/Content.module.css';
 
 function Content({content, isWriter}) {
+  const [updateMode, setUpdateMode] = useState(false);
+  const [editText, setEditText] = useState(content.text);
+
   const contentFromDatabase = doc(fireDatabase, "contents", `${content.id}`);
 
   const deleteButtonHandler = async() => {
@@ -13,15 +16,46 @@ function Content({content, isWriter}) {
     }
   };
 
+  const toggleUpdate = () => {
+    setUpdateMode((current) => !current);
+  };
+
+  const editTextChange = ({target : {value}}) => {
+    setEditText(value);
+  };
+
+  const formSubmit = async(e) => {
+    e.preventDefault();
+    await updateDoc(contentFromDatabase, {text: editText});
+    setUpdateMode(false);
+  };
+
   return (
     <div>
-      {/* 작성자와 로그인 중인 사용자가 같을 때 */}
-      {isWriter && 
-        <div className={style.content_button}>
-          <button type="button">수정</button>
-          <button type="button" onClick={deleteButtonHandler}>삭제</button>
-        </div>
-      }
+      {updateMode ?
+        <> 
+          {isWriter && 
+            <div className={style.update_form}>
+              <form onSubmit={formSubmit}>
+                <input type="text" value={editText} onChange={editTextChange} required/>
+                <button type="submit">수정하기</button>
+              </form>
+              <button onClick={toggleUpdate}>취소</button>
+            </div>
+          }
+        </>
+        :
+        <>
+          <h4 className={style.title}>{content.text}</h4>
+          {/* 작성자와 로그인 중인 사용자가 같을 때 */}
+          {isWriter && 
+            <div className={style.content_button}>
+              <button type="button" onClick={toggleUpdate}>수정</button>
+              <button type="button" onClick={deleteButtonHandler}>삭제</button>
+            </div>
+          }
+        </>
+        }
     </div>
   )
 }
