e2a0053a3bc80c50a66c6fef20180b65f95b1b87
leenawon <nawon5154@gmail.com>
2021-12-26 22:36:04 +0900

    Profile page에 currentUserName 함수 props로 전달

--- src/components/Router.js
+++ src/components/Router.js
@@ -5,7 +5,7 @@ import Home from 'pages/Home';
 import Navigation from 'components/Navigation';
 import Profile from 'pages/Profile';
 
-const AppRouter = ({login, userObject}) => {
+const AppRouter = ({login, userObject, currentUserName}) => {
   return (
     <Router>
       {login && <Navigation userObject={userObject}/>}
@@ -14,7 +14,7 @@ const AppRouter = ({login, userObject}) => {
           login ?
           <>
             <Route exact path="/" element={<Home userObject={userObject}/>} />
-            <Route exact path="/profile" element={<Profile userObject={userObject}/>} />
+            <Route exact path="/profile" element={<Profile userObject={userObject} currentUserName={currentUserName}/>} />
           </> 
           : <Route exact path="/" element={<Auth/>} />
         }
