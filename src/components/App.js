53f026854298ed5c1bae8ff3962f41ff867f0429
leenawon <nawon5154@gmail.com>
2021-12-26 22:24:09 +0900

    Profile page에서 변경된 사용자 이름을 프로젝트 화면에도 적용시키기 위해 currentUserName props 전달

--- src/components/App.js
+++ src/components/App.js
@@ -12,7 +12,12 @@ function App() {
     // Auth에 대한 변화를 감지함
     firebaseAuth.onAuthStateChanged((user) => {
       if(user) {
-        setUserObject(user);
+        // user의 displayName, uid, updateProfile만 userObject에 넣어줌
+        setUserObject({
+          displayName: user.displayName ? user.displayName : "사용자",
+          uid: user.uid,
+          updateProfile: (argument) => user.updateProfile(argument)
+        });
       } else {
         setUserObject(null);
       }
@@ -20,10 +25,20 @@ function App() {
     });
   }, []);
 
+  // project 내의 displayName을 변경시켜줌
+  const currentUserName = () => {
+    const user = firebaseAuth.currentUser;
+    setUserObject({
+      displayName: user.displayName,
+      uid: user.uid,
+      updateProfile: (argument) => user.updateProfile(argument)
+    });
+  };
+
   return (
     <div className="app">
       <h3 className="title">🌳 Book-ggum-na-mu 🌳</h3>
-      {firebaseListen ? <AppRouter login={Boolean(userObject)} userObject={userObject}/> : "로그인 정보를 불러오는 중..."}
+      {firebaseListen ? <AppRouter login={Boolean(userObject)} userObject={userObject} currentUserName={currentUserName}/> : "로그인 정보를 불러오는 중..."}
     </div>
   );
 }
