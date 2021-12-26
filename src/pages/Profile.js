9bdeab99fae61b872d72e2c3a6b79ebe8c503b5a
leenawon <nawon5154@gmail.com>
2021-12-26 21:39:13 +0900

    Profile page에서 사용자가 이름을 변경할 수 있도록 form 만들고 기능 제작

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,18 +1,39 @@
 import { firebaseAuth } from 'myFirebase';
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { updateProfile } from 'firebase/auth';
 import style from 'css/Profile.module.css';
 
-function Profile() {
+function Profile({userObject}) {
+  const [userName, setUserName] = useState(userObject.displayName);
   const navigate = useNavigate();
   const handleLogoutButton = () => {
     firebaseAuth.signOut();
     navigate('/');
   };
+
+  const userNameChange = ({target: {value}}) => {
+    setUserName(value);
+  };
+
+  const formSubmit = async(e) => {
+    e.preventDefault();
+    // firebase Auth의 displayName을 변경시켜줌
+    if(userObject.displayName !== userName) {
+      await updateProfile(firebaseAuth.currentUser, {
+        displayName: userName
+      });
+    }
+    navigate('/');
+  };
   
   return (
     <div>
-      <h4>사용자의 Profile</h4>
+      <h4>{userObject.displayName}의 Profile</h4>
+      <form onSubmit={formSubmit}>
+        <input className={style.name_input} type="text" placeholder="이름" value={userName} onChange={userNameChange}/>
+        <button className={style.edit_button} type="submit">수정하기</button>
+      </form>
       <button className={style.logout_button} onClick={handleLogoutButton}>로그아웃</button>
     </div>
   )
